f6a42c121ddcd262800030719128c15e204d50ed
hiten26 <hitendravaghela91@gmail.com>
2023-07-25 10:41:38 +0200

    Logistic regression added

---
+++ Logistic Regression/logistic regression.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.01, num_iterations=1000):
+        self.learning_rate = learning_rate
+        self.num_iterations = num_iterations
+        self.weights = None
+        self.bias = None
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def fit(self, X, y):
+        # Initialize weights and bias
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # Gradient Descent
+        for _ in range(self.num_iterations):
+            linear_model = np.dot(X, self.weights) + self.bias
+            y_predicted = self.sigmoid(linear_model)
+
+            # Calculate gradients
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+
+            # Update parameters
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = self.sigmoid(linear_model)
+        return (y_predicted > 0.5).astype(int)
+
+if __name__ == "__main__":
+    # Sample dataset for binary classification
+    X_train = np.array([[1.0, 2.0], [2.0, 3.0], [3.0, 4.0], [4.0, 5.0], [5.0, 6.0]])
+    y_train = np.array([0, 0, 1, 1, 1])
+
+    X_test = np.array([[6.0, 7.0], [7.0, 8.0]])
+
+    # Create and train the logistic regression model
+    lr_model = LogisticRegression()
+    lr_model.fit(X_train, y_train)
+
+    # Make predictions
+    predictions = lr_model.predict(X_test)
+
+    print("Predictions:", predictions)
